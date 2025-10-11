dfaa5b823d24e80e26640a2c7bfeea282c95f677
JUNG hyein <hye__n@snu.ac.kr>
2025-10-11 18:24:44 +0900

    hall asset

---
+++ Assets/Scripts/HallControll/HallplayerControll.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HallplayerControll : MonoBehaviour
+{
+    private InputSystem_Actions input;
+    public float moveSpeed = 5f;
+    private Camera mainCamera;
+    private Vector2 moveInput;
+    private Vector3 targetPosition;
+    private bool movingToClick = false;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        input = new InputSystem_Actions ();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        input.Player.Enable();
+        input.Player.Move.performed += OnMovePerformed;
+        input.Player.Move.canceled += OnMoveCanceled;
+        input.Player.Click.performed += OnClickPerformed;
+    }
+
+
+    private void OnDisable()
+    {
+        input.Player.Disable();
+        input.Player.Move.performed -= OnMovePerformed;
+        input.Player.Move.canceled -= OnMoveCanceled;
+        input.Player.Click.performed -= OnClickPerformed;
+        
+    }
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        movingToClick = false; // 수동 이동 시 클릭 이동 중단
+    }
+    
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero; // 키보드 입력 끝나면 이동 멈춤
+    }
+    
+    private void OnClickPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 mouseScreenPos = input.Player.Point.ReadValue<Vector2>();
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos);
+        worldPos.z = transform.position.z;
+
+        targetPosition = worldPos;
+        movingToClick = true;
+    }
+    
+    private void Update()
+    {
+        if (Mathf.Abs(moveInput.x) > 0.01f)
+        {
+            movingToClick = false;
+            transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
+
+            if (moveInput.x > 0) spriteRenderer.flipX = false; 
+            else if (moveInput.x < 0) spriteRenderer.flipX = true;
+        }
+        else if (movingToClick)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            if (targetPosition.x < transform.position.x) spriteRenderer.flipX = true;
+            else if (targetPosition.x > transform.position.x) spriteRenderer.flipX = false;
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+                movingToClick = false;
+        }
+    }
+}
