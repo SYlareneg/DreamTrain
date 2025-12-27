621813f62b4e147ce9ddfe52f56e96684719b8bb
Minarrii <mwj1014@naver.com>
2025-12-28 02:42:32 +0900

    Encounter csv file upload
    인카운터 페이지에 쓸 인카운터의 csv파일을 업로드했습니다.

--- Assets/Scripts/BugChcker.cs
+++ Assets/Scripts/BugChcker.cs
@@ -12,12 +12,6 @@ public class BugChecker : MonoBehaviour
     void Update()
     {
         int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = 6;
-        int g = 2
           ;
           
      
