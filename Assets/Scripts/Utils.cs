09416920510ab2adcee16e568175e5a21b926906
SYlareneg <dbtlswkdrnsss@gmail.com>
2026-01-23 11:25:24 +0900

    bug fix 3

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -189,7 +189,15 @@ public class Utils
         var dir = Path.GetDirectoryName(dst);
         if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
 
-        // if (File.Exists(dst)) yield break; // 유저 파일이 이미 있으면 그대로 사용
+        // 유저 파일이 이미 있으면 그대로 사용
+		if(relativePath.Contains("player_data.json") || relativePath.Contains("player_data_start.json"))
+		{
+			if (File.Exists(dst))
+			{
+				done?.Invoke(true);
+				yield break;
+			}
+		}
 
         var src = Path.Combine(Application.streamingAssetsPath, "Data", relativePath);
 
